b2a8beacefd16863d6e4d08c48f5becefb9c83ac
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2021-03-25 17:45:55 +0300

    error indicator

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -6,7 +6,7 @@ const ErrorIndicator = () => {
   return (
     <div className='error'>
       <img src={errorImg} width='100px' height='100px'/>
-      <p>Sorry,something was terribly wrong</p>
+      <p>Sorry, something was terribly wrong  : (</p>
     </div>
   )
 }
