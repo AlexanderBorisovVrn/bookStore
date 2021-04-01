830aef79e353bb0ffd783634206da47ffeb1d354
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2021-04-01 15:31:12 +0300

    add styles for cards

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -2,23 +2,29 @@ import React from 'react';
 import './book-list-item.css'
 
 const BookListItem = ({book}) => {
-  const {authors, title, img} = book;
+  const {authors, title, img, cost} = book;
+  const displayAuthors = (authors) => {
+    return authors.map(author => {
+      return <p>{author}</p>
+    })
+  }
+  console.log(displayAuthors(authors));
   return (
     <div className='book-list-item'>
       <img className='book-list-item__img' alt='' src={img}/>
-      <ul className='about'>
-        <li className='about__title'>
-          <button hrefLang='#'>
-            {title}
-          </button>
-        </li>
-        <li className='about__authors'>
-          <button >{authors}</button>
-        </li>
-      </ul>
+      <div className='about'>
+        <button className='about__title'>
+          {title}
+        </button>
+        <div className='about__authors'>
+          {displayAuthors(authors)}
+        </div>
+        <div className='about__cost'>
+          <span>{`$${cost}`}</span>
+        </div>
+        <button className='btn'>Add to cart</button>
+      </div>
     </div>
-
   )
 }
-
-export default BookListItem;
+export default BookListItem
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -8,13 +8,16 @@ export default class BookStoreService {
           "David A. Vise", "Mark Malseed"
         ],
         "id": '1',
-        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg"
+        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg",
+        "cost":"400"
       }, 
       {
         "id": '2',
         "title": "Best of me",
         "authors": ["Nicholas Sparks"],
-        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg"
+        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg",
+        "cost":"41"
+
       },
       {
         "title": "The Google story",
@@ -22,13 +25,15 @@ export default class BookStoreService {
           "David A. Vise", "Mark Malseed"
         ],
         "id": '3',
-        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg"
+        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg",
+        "cost":"14"
       }, 
       {
         "id": '4',
         "title": "Best of me",
         "authors": ["Nicholas Sparks"],
-        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg"
+        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg",
+        "cost":"17"
       },
       {
         "title": "The Google story",
@@ -36,19 +41,24 @@ export default class BookStoreService {
           "David A. Vise", "Mark Malseed"
         ],
         "id": '5',
-        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg"
+        "img": "https://cdn1.ozone.ru/multimedia/1010296914.jpg",
+        "cost":"5"
       }, 
       {
         "id": '6',
         "title": "Best of me",
         "authors": ["Nicholas Sparks"],
-        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg"
+        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg",
+        "cost":"25"
+
       },
       {
         "id": '7',
         "title": "Best of me",
         "authors": ["Nicholas Sparks"],
-        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg"
+        "img": "http://s.s-bol.com/imgbase0/imagebase/large/FC/4/6/2/8/9200000001208264.jpg",
+        "cost":"4"
+
       }
     ];
   }
