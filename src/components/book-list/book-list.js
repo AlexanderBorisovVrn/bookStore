9f5567c6d3f23ef208d366be3c69f71573c155ea
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2021-04-02 15:23:33 +0300

    add spinner

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,27 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useState} from 'react';
 import BookListItem from '../book-list-item';
 import {connect} from 'react-redux';
 import {booksLoaded} from '../../actions/actions'
 import {withBookstoreServiceContext} from '../hoc';
+import Spinner from '../spinner'
 import './book-list.css'
 
 const BookList = ({books, bookstoreService, booksLoaded}) => {
-
+  
+  const [state, setState] = useState({loading:true});
   useEffect(() => {
-    const data = bookstoreService.getBooks();
-    booksLoaded(data)
+  bookstoreService
+    .getBooks()
+      .then(data=>{
+        setState({loading:false})
+        booksLoaded(data)
+      })
   }, [bookstoreService, booksLoaded]);
-
-  return (
-    <ul className='book-list'>
+  
+  if(state.loading){
+    return <div>
+      <Spinner/>
+    </div>
+  }
+ else{ return (
+        <ul className='book-list'>
       {books.map(book => {
         return <li key={book.id}>
           <BookListItem book={book}/>
         </li>
       })
-}
+    }
     </ul>
-  )
+  )}
 }
 
 const mapStateToProps = ({books}) => {
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+      <div className="cssload-loader">Загрузка</div>
+  )
+}
+
+export default Spinner;
